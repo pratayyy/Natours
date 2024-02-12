c4426c31935b7222ae4a92e600423672311c19aa
pratayyy <pratayroy_official@yahoo.com>
2024-02-13 00:23:32 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfull!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}....`);
+});
