79755c6e1183dc86a7f4e70b3f43f90d512e135f
pratayyy <pratayroy_official@yahoo.com>
2024-02-18 14:19:53 +0530

    Reset token generation & sending it via mail in forgetPassword method

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
@@ -30,7 +31,7 @@ const userSchema = new mongoose.Schema({
     type: 'string',
     required: [true, 'Please confirm your password'],
     validate: {
-      // This only wors on CREATE & SAVE
+      // This only works on CREATE & SAVE
       validator: function (el) {
         return el === this.password;
       },
@@ -38,6 +39,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: 'string',
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -68,6 +71,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   }
 
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
