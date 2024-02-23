b6f80e04f5820d55ced24925cb4dfb3e7253eb09
pratayyy <pratayroy_official@yahoo.com>
2024-02-23 19:10:19 +0530

    Added nested routes for tour reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// POST /tour/32434fs35/reviews
+// GET /tour/32434fs35/reviews
+// GET /tour/32434fs35/reviews/97987dssad8
+
+// router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview, );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
