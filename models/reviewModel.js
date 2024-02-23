ad5fd9b13d4056c8933d8aa88213a04663b2f999
pratayyy <pratayroy_official@yahoo.com>
2024-02-23 18:22:04 +0530

    Virtually populated tours & reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,10 +33,16 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
+  // this.populate({
+  //   path: 'tour',
+  //   select: 'name',
+  // }).populate({
+  //   path: 'user',
+  //   select: 'name photo',
+  // });
+  // next();
+
   this.populate({
-    path: 'tour',
-    select: 'name',
-  }).populate({
     path: 'user',
     select: 'name photo',
   });
