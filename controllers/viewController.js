cf9e223e662e78c698b47cb0c31f558c49675b85
pratayyy <pratayroy_official@yahoo.com>
2024-02-25 23:14:08 +0530

    Build the tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -13,8 +13,17 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Getthe data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating name',
   });
-};
+
+  // 2) Build template
+  // 3) Render that templaye using the data from 1
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
