b9349c97967a907824f85f8d336611a3a53faedd
pratayyy <pratayroy_official@yahoo.com>
2024-02-27 18:13:41 +0530

    Signup functionality from frontend added

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,12 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login } from './login';
+import { signup } from './signup';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
+const signUpFrom = document.querySelector('.form--signup');
 
 // DELEGATION
 if (mapBox) {
@@ -19,5 +21,16 @@ if (loginForm) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
+  });
+}
+
+if (signUpFrom) {
+  signUpFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
   });
 }
