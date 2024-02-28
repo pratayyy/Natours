ff85b20fed13cc570aebdbf6ceaa2dc7b5380810
pratayyy <pratayroy_official@yahoo.com>
2024-02-28 15:31:52 +0530

    Update user data with api

---
+++ public/js/updateSettings.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alert';
+
+export const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
+      data: {
+        name,
+        email,
+      },
+    });
+
+    console.log(res.data.status);
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Data updated successfully!');
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
