c1744e4a53f60920cb78801492f0e3e2d7dac892
pratayyy <pratayroy_official@yahoo.com>
2024-03-01 16:33:51 +0530

    Saving image name to database

--- models/userModel.js
+++ models/userModel.js
@@ -5,30 +5,33 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
-    type: 'string',
+    type: String,
     required: [true, 'Please tell us your name!'],
   },
   email: {
-    type: 'string',
+    type: String,
     required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: 'string',
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
   role: {
-    type: 'string',
+    type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
     default: 'user',
   },
   password: {
-    type: 'string',
+    type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
     select: false,
   },
   passwordConfirm: {
-    type: 'string',
+    type: String,
     required: [true, 'Please confirm your password'],
     validate: {
       // This only works on CREATE & SAVE
@@ -39,10 +42,10 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
-  passwordResetToken: 'string',
+  passwordResetToken: String,
   passwordResetExpires: Date,
   active: {
-    type: 'boolean',
+    type: Boolean,
     default: true,
     select: false,
   },
