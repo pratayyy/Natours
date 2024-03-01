1ecb14672202bdb0624d041e1390c076a74770fb
pratayyy <pratayroy_official@yahoo.com>
2024-03-01 22:39:56 +0530

    Password reset functionality updated

--- utils/email.js
+++ utils/email.js
@@ -54,4 +54,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the natours family!');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (Valid for 10 mins)',
+    );
+  }
 };
