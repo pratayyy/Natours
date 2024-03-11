55ed086ef9134984e5325e2858e19a95761c6f95
pratayyy <pratayroy_official@yahoo.com>
2024-03-11 19:00:38 +0530

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,7 @@ import { login, logout } from './login';
 import { signup } from './signup';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alert';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -82,3 +83,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
