2e620fbefe5baf34cf7d6395a9e184d03cc88eed
pratayyy <pratayroy_official@yahoo.com>
2024-03-11 18:11:57 +0530

    Implemented stripe webhooks

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
 app.enable('trust proxy');
+app.set('trust proxy', false);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -102,6 +104,12 @@ const limiter = rateLimiter({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout,
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
